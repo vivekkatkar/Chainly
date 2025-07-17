a1f0b50f9a072bcc0679af4db489793743979081
vivekkatkar <katkarvivek0@gmail.com>
2025-07-17 06:38:59 +0530

    Final commit

--- worker/src/email.ts
+++ worker/src/email.ts
@@ -1,4 +1,21 @@
+import nodemailer from "nodemailer";
 
-export function sendEmail(to : string, body : string){
-    
+const transport = nodemailer.createTransport({
+    host: process.env.SMTP_ENDPOINT,
+    port: 587,
+    secure: false, 
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+export async function sendEmail(to: string, body: string) {
+    await transport.sendMail({
+        from: "contact@100xdevs.com",
+        sender: "contact@100xdevs.com",
+        to,
+        subject: "Hello from Zapier",
+        text: body
+    })
 }
